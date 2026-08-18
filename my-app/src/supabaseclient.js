875d8408f2fd2a70a7a55b869ad256b0d7a827a7
Saba Zubair <saba214214@gmail.com>
2026-08-18 16:24:34 +0500

    app

--- my-app/src/supabaseclient.js
+++ my-app/src/supabaseclient.js
@@ -1,8 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseURL=import.meta.env.VITE_SUPABASE_URL;
-const myAPIkey=import.meta.env.VITE_MY_API_KEY;
-
-
-
-
-export const myDatabase=createClient(supabaseURL, myAPIkey)
+const SUPABASE_URL=import.meta.env.VITE_SUPABASE_URL;
+const MY_API_KEY=import.meta.env.VITE_MY_API_KEY;
+export const myDatabase=createClient(SUPABASE_URL, MY_API_KEY)
